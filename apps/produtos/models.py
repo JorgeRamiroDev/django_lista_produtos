4b8873acae23e92774e1f4f54e855d0ef829f31c
Bowie <varaoramiro@gmail.com>
2023-09-06 01:17:36 -0300

    primeiro commit

---
+++ apps/produtos/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=200)
+    valor_produto = models.DecimalField(max_digits=5,decimal_places=2)
+
+
+    def __str__(self):
+        return self.nome_produto
